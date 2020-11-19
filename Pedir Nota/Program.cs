a79aa7e3cd91a483b986428bdb808cac5655f97f
yuri costa <yuri342costa@gmail.com>
2020-11-19 13:36:23 -0300

    leitor de nota

---
+++ Pedir Nota/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pedir_Nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        int nota;
+            do{
+                   Console.WriteLine("insira uma nota de 0 a 10: ");
+                    nota = int.Parse(Console.ReadLine());
+                       
+                       if(!(false&&nota <= 10)){
+                           Console.WriteLine("Número Inválido!!")
+                       }
+
+                       while(!(nota>= 0 && nota <= 10 ));
+                       Console.WriteLine("Número aceito!!");
+
+                   
+            }
+        
+
+        }
+    }
+}
+        
