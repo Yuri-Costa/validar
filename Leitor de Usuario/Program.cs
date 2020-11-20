2d64a135a3811cfeb266e8d8ba81516662e25b35
yuri costa <yuri342costa@gmail.com>
2020-11-19 22:44:16 -0300

    finish

---
+++ Leitor de Usuario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Leitor_de_Usuario
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+
+            string username;
+            string senha; 
+
+            do{
+                Console.WriteLine("digite seu username:");
+                username = Console.ReadLine();
+
+                Console.WriteLine("digite sua senha:");
+                senha = Console.ReadLine();
+
+                if(senha == username){ 
+                Console.WriteLine("a senha não pode ser igual o nome de usuario:");
+
+                }
+
+            }while(senha == username);
+            Console.WriteLine("usuario e senha aceitos!!");
+              
+
+        }
+    }
+}
---
+++ validar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace validar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { string nome;
+            int idade;
+            double salario;
+            char estadocivil;
+           
+           do{
+               Console.WriteLine("insira seu nome: ");
+               nome = Console.ReadLine(); 
+
+
+           }while(nome =="" || nome=="");
+
+           do{
+               Console.WriteLine("insira sua idade: ");
+               idade = int.Parse(Console.ReadLine()); 
+
+
+        }while(idade <= 0 || idade > 150);
+
+        do{
+               Console.WriteLine("insira seu salario: ");
+               salario = double.Parse(Console.ReadLine());
+
+    }while( salario > 0);
+
+     do{
+               Console.WriteLine("insira seu estado civil : "s" (solteiro), "c" (casado), "d" (divorsiado) e "v" (viuvo)");
+                string estado = Console.ReadLine();
+               estadocivil = estado.ToCharArray()[0];
+               
+}while(estadocivil!= "S" && estadocivil!= "c" && estadocivil!= "d" && estadocivil!= "v");
+Console.WriteLine("valores aceitos");
+        }
+    }
